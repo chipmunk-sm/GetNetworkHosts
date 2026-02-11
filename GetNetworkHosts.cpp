bf33af74a7ddaddca81205337b6af1d8d99e62f6
chipmunk.sm <dannico@linuxmail.org>
2026-02-11 13:35:48 +0100

    refactoring/improve progress

--- GetNetworkHosts.cpp
+++ GetNetworkHosts.cpp
@@ -1,4 +1,4 @@
-#ifndef WIN32_LEAN_AND_MEAN
+﻿#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
 
@@ -29,94 +29,80 @@
 #include "HostItem.h"
 #include "HostCollector.h"
 
-int wmain(int argc, wchar_t argv[])
-{
-
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv[0]);
-
-	try
-	{
-
-		WSAData d;
+class WinsockContext {
+public:
+	WinsockContext() {
+		WSADATA d;
 		if (WSAStartup(MAKEWORD(2, 2), &d) != 0) {
-			return -1;
-		}
-
-		auto xStartTime = std::chrono::high_resolution_clock::now();
-
-		HostCollector collector;
-		//collector.CollectNeighborIp(AF_INET);
-		//collector.CollectNeighborIp(AF_INET6);
-		//collector.CollectNeighborIp(AF_UNSPEC);
-		//collector.CollectUnicastIp(AF_INET);
-		//collector.CollectUnicastIp(AF_INET6);
-		//collector.CollectUnicastIp(AF_UNSPEC);
-
-		collector.CollectNeighborIp();
-		collector.CollectUnicastIp();
-		collector.CollectAnycastIp();
-		collector.CollectMulticastIp();
-		
-		{
-			auto xLastTime = xStartTime;
-			while (collector.WaitForResult(std::chrono::milliseconds(10)) > 0 && !collector.IsAbort()) {
-				const auto xCurrentTime = std::chrono::high_resolution_clock::now();
-				const auto dInterval = std::chrono::duration_cast<std::chrono::duration<double>>(xCurrentTime - xLastTime).count();// 
-				if (dInterval > 2.5) {
-					const auto dElapsed = std::chrono::duration_cast<std::chrono::duration<double>>(xCurrentTime - xStartTime).count();// 
-					fwprintf(stdout, L"\rElapsed %9.6f sec.          ", dElapsed);
-					fflush(stdout);
-					xLastTime = xCurrentTime;
-				}
-			}
-		}
-
-		const auto dReady = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - xStartTime).count();// 
-
-
-#ifdef TRACEENABLE
-		std::wstring out(L"--- START ---\n");
-		OutputDebugStringW(out.c_str());
-#endif
-
-		fwprintf(stdout, L"\n--- START --- \n");
-
-		for (auto& item : collector.GetHostList()) {
-			//            IP      HOS    FAM    ADA    TYP
-			fwprintf(stdout, L"| %s%s | %s%s | %s%s | %s%s | %s%s | %s\n",
-				item.Ip.c_str(), std::wstring(collector.GetPaddingLenIp(item.Ip), ' ').c_str(),
-				item.Host.c_str(), std::wstring(collector.GetPaddingLenHost(item.Host), ' ').c_str(),
-				item.Family.c_str(), std::wstring(collector.GetPaddingLenFamily(item.Family), ' ').c_str(),
-				item.GetAdaptersNames().c_str(), std::wstring(collector.GetPaddingLenAdapter(item.GetAdaptersNames()), ' ').c_str(),
-				item.IfType.c_str(), std::wstring(collector.GetPaddingLenType(item.IfType), ' ').c_str(),
-				item.GetCollectorTypes().c_str()
-			);
-		}
-
-		fwprintf(stdout, L"--- END --- Collect in %9.6f sec.\n", dReady);
-
-	}
-	catch (const std::exception& ex) {
-		fprintf(stderr, "FATAL: %s", ex.what());
-	}
-	catch (...) {
-		try {
-			std::rethrow_exception(std::current_exception());
-		}
-		catch (const std::exception& ex) {
-			fprintf(stderr, "FATAL: %s", ex.what());
-		}
-		catch (...) {
-			fprintf(stderr, "FATAL: Unexpected exception");
+			throw std::runtime_error("WSAStartup failed");
 		}
 	}
+	~WinsockContext() {
+		WSACleanup();
+	}
+	// Запрещаем копирование
+	WinsockContext(const WinsockContext&) = delete;
+	WinsockContext& operator=(const WinsockContext&) = delete;
+};
 
-	WSACleanup();
+int main() {
+    try {
 
-	return 0;
+        auto xStartTime = std::chrono::high_resolution_clock::now();
 
+        WinsockContext wsContext;
 
+        HostCollector collector;
 
+        collector.CollectNeighborIp();
+        collector.CollectUnicastIp();
+        collector.CollectAnycastIp();
+        collector.CollectMulticastIp();
+
+        auto xLastProgressUpdate = xStartTime;
+        const auto progressThreshold = std::chrono::milliseconds(500);
+
+        while (collector.WaitForResult(std::chrono::milliseconds(20)) > 0 && !collector.IsAbort()) {
+            auto xCurrentTime = std::chrono::high_resolution_clock::now();
+
+            if (xCurrentTime - xLastProgressUpdate > progressThreshold) {
+                auto dElapsed = std::chrono::duration<double>(xCurrentTime - xStartTime).count();
+
+                fwprintf(stdout, L"\rElapsed %9.6f sec.          ", dElapsed);
+                fflush(stdout);
+
+                xLastProgressUpdate = xCurrentTime;
+            }
+        }
+
+        auto xEndTime = std::chrono::high_resolution_clock::now();
+        double dTotalTime = std::chrono::duration<double>(xEndTime - xStartTime).count();
+
+        fwprintf(stdout, L"\n--- START --- \n");
+
+        for (auto& item : collector.GetHostList()) {
+            //            IP      HOS    FAM    ADA    TYP
+            fwprintf(stdout, L"| %s%s | %s%s | %s%s | %s%s | %s%s | %s\n",
+                item.Ip.c_str(), std::wstring(collector.GetPaddingLenIp(item.Ip), ' ').c_str(),
+                item.Host.c_str(), std::wstring(collector.GetPaddingLenHost(item.Host), ' ').c_str(),
+                item.Family.c_str(), std::wstring(collector.GetPaddingLenFamily(item.Family), ' ').c_str(),
+                item.GetAdaptersNames().c_str(), std::wstring(collector.GetPaddingLenAdapter(item.GetAdaptersNames()), ' ').c_str(),
+                item.IfType.c_str(), std::wstring(collector.GetPaddingLenType(item.IfType), ' ').c_str(),
+                item.GetCollectorTypes().c_str()
+            );
+        }
+
+        fwprintf(stdout, L"--- END --- Collect in %9.6f sec.\n", dTotalTime);
+
+    }
+    catch (const std::exception& ex) {
+        fprintf(stderr, "\nFATAL ERROR: %s\n", ex.what());
+        return -1;
+    }
+    catch (...) {
+        fprintf(stderr, "\nFATAL ERROR: Unknown exception occurred\n");
+        return -1;
+    }
+
+    return 0;
 }
-
